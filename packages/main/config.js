096767b4075a9ed0111cdef9512ee11c5448e133
li-yinan <biline@163.com>
2018-03-25 23:09:50 +0800

    增加.vpsrc配置机制

--- packages/main/config.js
+++ packages/main/config.js
@@ -1,9 +1,23 @@
 import path from 'path';
+import fs from 'fs';
 
-export const baseDir = path.resolve(__dirname);
+let runPath = process.cwd();
 
-export const baseClassPath = path.resolve('./base');
+let customConfig = {};
 
-export const defaultClassName = 'defaultexport';
+let rcPath = path.resolve(runPath, '.vpsrc');
 
-export const outputPath = path.resolve('./output');
+if (fs.existsSync(rcPath)) {
+    customConfig = JSON.parse(fs.readFileSync(rcPath));
+}
+
+// short name
+let cc = customConfig;
+
+export const baseDir = cc.baseDir || path.resolve(__dirname);
+
+export const baseClassPath = cc.baseClassPath || path.resolve('./base');
+
+export const defaultClassName = cc.defaultClassName || 'defaultexport';
+
+export const outputPath = cc.outputPath ? path.resolve(runPath, cc.outputPath) : path.resolve('./output');

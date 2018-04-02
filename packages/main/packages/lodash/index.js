c88bc3598e7677966f6949ba4a19014201c0551e
li-yinan <biline@163.com>
2018-04-02 19:42:26 +0800

    增加find函数，测试数据由unicode转为汉字

--- packages/main/packages/lodash/index.js
+++ packages/main/packages/lodash/index.js
@@ -1,4 +1,13 @@
 export default function () {
 };
+
 export function chunk(arr, count) {
+};
+
+export function find(arr, iterator) {
+    for (var data of arr) {
+        if (iterator(data)) {
+            return data;
+        }
+    }
 };

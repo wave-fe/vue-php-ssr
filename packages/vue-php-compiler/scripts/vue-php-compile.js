c2ea23843ef88b02d845d8a82e0965d1d2ced624
li-yinan <biline@163.com>
2018-04-12 20:36:23 +0800

    增加bin

---
+++ packages/vue-php-compiler/scripts/vue-php-compile.js
@@ -0,0 +1,24 @@
+var commander = require('commander');
+var packageJSON = require('../package.json');
+var path = require('path');
+
+var compiler = require('../src/index');
+
+var runPath = process.cwd();
+
+commander.version(packageJSON.version)
+    .option('-r --recursive', 'auto find dependencies and compile')
+    .parse(process.argv);
+
+if (!commander.args.length) {
+    commander.outputHelp();
+    process.exit();
+}
+
+let filePath = path.resolve(runPath, commander.args[0]);
+if (commander.recursive) {
+    compiler.recursiveCompile(filePath);
+}
+else {
+    compiler.compile(filePath);
+}

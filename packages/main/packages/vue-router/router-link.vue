050af9b4ae85ecf916bde16d39192a384286ace7
li-yinan <biline@163.com>
2018-04-12 14:41:07 +0800

    增加了router-link

--- packages/main/packages/vue-router/router-link.vue
+++ packages/main/packages/vue-router/router-link.vue
@@ -1,12 +1,31 @@
 <template>
-    <a></a>
+    <a :href="addr"><slot></slot></a>
 </template>
 
 <script>
 export default {
+    props: ['to'],
     data() {
         return {
             type: ''
+        }
+    },
+    computed: {
+        addr() {
+            if (typeof this.to === 'array') {
+                let str = '/' + this.to['name'];
+                if ('params' in this.to) {
+                    let params = this.to['params'];
+                    str = str + '?';
+                    for (var key in params) {
+                        str = str + key + '=' + params[key];
+                    }
+                }
+                return str;
+            }
+            else {
+                return this.to;
+            }
         }
     }
 }

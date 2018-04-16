3c25964e49a6d33ec0b2bfc31a3883fa06360b7d
li-yinan <biline@163.com>
2018-04-16 16:29:13 +0800

    增加对php版本的判断

--- packages/vue-php-demo/basic/scripts/start.sh
+++ packages/vue-php-demo/basic/scripts/start.sh
@@ -1,5 +1,22 @@
 set -e
 
+# 检测php命令是否存在
+command -v php -v >/dev/null 2>&1 || {
+    echo >&2 "php required but it's not installed.  Aborting.";
+    exit 1;
+}
+
+function version_gt() {
+    test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1";
+}
+
+PHP_VERSION=`php -v|head -1|awk '{print $2}'`;
+
+if version_gt '5.6.0' $PHP_VERSION; then
+   echo "php version is too old, please use v5.6.0+"
+   exit 1;
+fi
+
 # start php server
 php -S 0.0.0.0:8090&
 
